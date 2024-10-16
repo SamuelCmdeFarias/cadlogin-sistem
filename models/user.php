db7bb38936e6c5e4641b674c00f37258f362b93f
SamuelCmdeFarias <samucacsg@gmail.com>
2024-10-16 07:52:07 -0300

    add uma função de listagem dos usuarios

--- models/user.php
+++ models/user.php
@@ -5,23 +5,32 @@ require_once 'models/database.php';
 class user
 {
     public static function FindByEmail($email){
-        $comn = Database::getComnction();
-        $stmt = $comn->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
         $stmt->execute(['email' => $email]);
         return $stmt->Fetch(PDO::FETCH_ASSOC);
     }
  
     public static function Find($id){
-        $comn = Database::getComnction();
-        $stmt = $comn->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
         $stmt->execute(['id' => $id]);
         return $stmt->Fetch(PDO::FETCH_ASSOC);
     }
  
     static public function create($data){
-        $comn = Database::getComnction();
-        $stmt = $comn->prepare("INSERT INTO usuarios(nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)");
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("INSERT INTO usuarios(nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)");
         $stmt->execute($data);
    
     }
+
+    //Função para buscar todos os usuários da base de dados
+    public static function all(){
+        $conn = Database::getConnection();
+        $stmt = $conn->query("SELECT * FROM usuarios");
+        //Retorna todos os usuarios com um array associativo
+        return $stmt->FetchAll(PDO::FETCH_ASSOC);
+    }
+
 }
